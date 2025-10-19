c87e6e33f9d50e1c3c854cc2bceec6245f6be2c2
Nantapop.Kj <nantapop.kj@gmail.com>
2025-10-19 13:38:53 +0700

    add docker-compose setup for local development

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "github.com/nantapop-kj/go-auth-service/db"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/nantapop-kj/go-auth-service/config"
+	"github.com/nantapop-kj/go-auth-service/db"
+)
 
 func main() {
 	database := db.ConnectDB()
 	db.AutoMigrate(database)
+	app := fiber.New()
+
+	port := config.Getenv("BACKEND_PORT", "3001")
+	err := app.Listen(":" + port)
+	if err != nil {
+		log.Fatal("Error starting server: ", err)
+	}
 }
